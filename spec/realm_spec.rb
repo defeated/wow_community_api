7fa8b8c179b418b21720d208d67316f00cc68a85
eddie cianci <defeated2k4@gmail.com>
2011-07-10 20:33:32 -0400

    fixed realm spec

--- spec/realm_spec.rb
+++ spec/realm_spec.rb
@@ -1,16 +1,15 @@
 require 'spec_helper'
 
 describe Realm do
-  describe "#status" do
+  describe "#find_by_name" do
     before do
-      BattleNet.region('us')
-      expected_uri = "http://us.battle.net/api/wow/realm/status?realms=arygos"
-      expected_body = '{"realms":[{ "type":"pve", "queue":false, "status":true, "population":"medium", "name":"Arygos", "slug":"arygos"}]}'
+      expected_uri = "http://us.battle.net/api/wow/realm/status?realms=medivh"
+      expected_body = '{ "realms":[{ "type":"pve", "queue":false, "status":true, "population":"medium", "name":"Medivh", "slug":"medivh" }] }'
       stub_request(:get, expected_uri).to_return(:body => expected_body, :headers => { 'Content-Type' => 'application/json' })
     end
     
     it "gets the status of a realm by name" do
-      realm = Realm.find_by_name("arygos")
+      realm = Realm.find_by_name("medivh")
       realm.status.should be_true
     end
   end
