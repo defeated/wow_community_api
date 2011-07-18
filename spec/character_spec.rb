fdca7f73f95810d567d5a7f45b90783e1af2342e
Matthew Gast <incendium@gmail.com>
2011-07-17 23:15:50 -0500

    Get Character and Realm in on the escaping action

--- spec/character_spec.rb
+++ spec/character_spec.rb
@@ -7,5 +7,13 @@ describe Character do
       character = Character.find_by_realm_and_name("uther", "malkyth")
       character.name.should == "Malkyth"
     end
+
+    it 'gets a character from a realm with spaces by realm and name' do
+      stub_json "http://us.battle.net/api/wow/character/altar of storms/johndoe", 'character-doe.json'
+
+      character = Character.find_by_realm_and_name('altar of storms', 'johndoe')
+      character.realm.should == 'Altar of Storms'
+      character.name.should == 'Johndoe'
+    end
   end
 end
--- spec/realm_spec.rb
+++ spec/realm_spec.rb
@@ -7,5 +7,12 @@ describe Realm do
       realm = Realm.find_by_name("medivh")
       realm.status.should be_true
     end
+
+    it 'gets the status of a realm with spaces by name' do
+      stub_json 'http://us.battle.net/api/wow/realm/status?realms=altar of storms', 'realms-aos.json'
+
+      realm = Realm.find_by_name("altar of storms")
+      realm.status.should be_true
+    end
   end
 end
