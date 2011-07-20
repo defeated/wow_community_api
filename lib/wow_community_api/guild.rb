5707422d2259c52ed3333be53db3dcbde8f80afb
eddie cianci <defeated2k4@gmail.com>
2011-07-19 21:16:13 -0400

    support optional members Guild field. closes #4

--- lib/wow_community_api/guild.rb
+++ lib/wow_community_api/guild.rb
@@ -1,7 +1,8 @@
 module WowCommunityApi
   class Guild < BattleNet
-    def self.find_by_realm_and_name(realm, name)
-      get("/guild/#{realm}/#{name}")
+    def self.find_by_realm_and_name(realm, name, *field)
+      fields = { :fields => field.join(",") } if field.size > 0
+      get("/guild/#{realm}/#{name}", :query => fields)
     end
   end
 end
--- spec/guild_spec.rb
+++ spec/guild_spec.rb
@@ -16,5 +16,22 @@ describe Guild do
       guild.realm.should == 'Altar of Storms'
       guild.name.should == 'Devilsaur May Cry'
     end
+    
+    it 'gets a guild with optional field members' do
+      stub_json 'http://us.battle.net/api/wow/guild/arygos/is%20epic?fields=members', 'guild-members.json'
+
+      guild = Guild.find_by_realm_and_name('arygos', 'is epic', :members)
+      guild.members.size.should == 3
+      guild.members[0].character.name.should == "Myrlidaren"
+    end
+    
+    it 'gets a guild with multiple optional fields' do
+      stub_json 'http://us.battle.net/api/wow/guild/arygos/is%20epic?fields=members,achievements', 'guild-all-fields.json'
+
+      guild = Guild.find_by_realm_and_name('arygos', 'is epic', :members, :achievements)
+      guild.members.size.should == 3
+      guild.members[0].character.name.should == "Myrlidaren"
+      guild.achievements.achievementsCompleted.size.should == 3
+    end
   end
 end
